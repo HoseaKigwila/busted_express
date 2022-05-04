5d9ddc91fc1f4fa9d44dff5e1bc13043c28b2921
Zay <hosea8zay@gmail.com>
2022-05-04 11:03:38 -0400

    fix on app.js hostName

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const hostname = '127.0.0.1';
+const hostName = '127.0.0.1';
 const port = 3000;
 
 const express = require('express'),
@@ -15,7 +15,7 @@ app.set('view engine', 'html');
 const server = http.createServer(app);
 
 server.listen(PORT, HOSTNAME, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
+    console.log(`Server running at http://${hostName}:${port}`)
 });
 
 const rootController = require('./routes/index');
@@ -23,3 +23,4 @@ const rangersController = require('./routes/rangers');
 
 
 app.use('/rangers', rangersController);
+app.use('/index', rootController);
